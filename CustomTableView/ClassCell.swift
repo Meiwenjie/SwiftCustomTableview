eadd13384e6547e791e1caca1970709130c8d0ad
MWJ <314002301@qq.com>
2016-03-08 20:43:25 +0800

    first commit

---
+++ CustomTableView/ClassCell.swift
@@ -0,0 +1,24 @@
+//
+//  ClassCell.swift
+//  CustomTableView
+//
+//  Created by admin on 16/3/3.
+//  Copyright © 2016年 MWJ. All rights reserved.
+//
+
+import UIKit
+
+class ClassCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CustomTableView/NibCell.swift
@@ -0,0 +1,24 @@
+//
+//  NibCell.swift
+//  CustomTableView
+//
+//  Created by admin on 16/3/3.
+//  Copyright © 2016年 MWJ. All rights reserved.
+//
+
+import UIKit
+
+class NibCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
